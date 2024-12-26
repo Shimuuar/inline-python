5a1d7de7a0b04e6f13bf8a7d94f45806e47e6577
Alexey Khudyakov <alexey.skladnoy@gmail.com>
2024-12-26 21:42:28 +0300

    "Preper" initialization
    There're a _lot_ of available knobs and argv handling seems to be wrong.
    But that will do for now

--- exe/tst.hs
+++ exe/tst.hs
@@ -3,9 +3,5 @@ module Main where
 import Python
 
 main :: IO ()
-main = do
-  print sizeof_PyConfig
-  -- 
-  cfg <- py_new_PyConfig
-  py_InitializeFromConfig cfg
+main = withPython $ do
   py_Foo
--- src/Python.hs
+++ src/Python.hs
@@ -7,9 +7,18 @@
 -- {-# OPTIONS_GHC -ddump-splices #-}
 module Python where
 
-import Foreign.Ptr
+import Control.Exception
+import Control.Monad.Trans.Cont
+import Control.Monad.IO.Class
+import Data.Char
 import Data.Map.Strict qualified as Map
-import Language.C.Inline qualified as C
+import Foreign.Ptr
+import Foreign.Marshal
+import Foreign.C.String
+import Foreign.C.Types
+import System.Environment
+
+import Language.C.Inline         qualified as C
 import Language.C.Inline.Context qualified as C
 
 
@@ -21,39 +30,71 @@ pyCtx = mempty { C.ctxTypesTable = Map.fromList tytabs } where
 --      , (TypeName "Rcomplex", [t| Complex Double |])
       ]
 
-
-
-C.include "<Python.h>"
+C.include "<inline-python.h>"
 
 ----------------------------------------------------------------
 -- Interpreter initialization
 ----------------------------------------------------------------
 
-sizeof_PyConfig :: Int
-sizeof_PyConfig = fromIntegral [C.pure| int { sizeof(PyConfig) } |]
+-- | Initialize python interpreter
+initializePython :: IO ()
+initializePython = do
+  -- NOTE: I'd like more direct access to argv
+  argv0 <- getProgName
+  argv  <- getArgs
+  let n_argv = fromIntegral $ length argv + 1
+  -- FIXME: For some reason sys.argv is initialized incorrectly. No
+  --        easy way to debug. Will do for now
+  r <- evalContT $ do
+    p_argv0  <- ContT $ withWCtring argv0
+    p_argv   <- traverse (ContT . withWCtring) argv
+    ptr_argv <- ContT $ withArray (p_argv0 : p_argv)
+    liftIO [C.block| int {
+      // Noop is interpreter is already initialized
+      if( Py_IsInitialized() ) {
+          return 0;
+      }
+      // Now fill config
+      PyStatus status;
+      PyConfig cfg;
+      PyConfig_InitPythonConfig( &cfg );
+      //--
+      status = PyConfig_SetBytesString(&cfg, &cfg.program_name, "XX");
+      if (PyStatus_Exception(status)) { goto error; }
+      //--
+      status = PyConfig_SetArgv(&cfg,
+          $(int       n_argv),
+          $(wchar_t** ptr_argv)
+      );
+      if( PyStatus_Exception(status) ) { goto error; };
+      // Initialize interpreter
+      status = Py_InitializeFromConfig(&cfg);
+      PyConfig_Clear(&cfg);
+      return PyStatus_Exception(status);
+      // Error case
+      error:
+      PyConfig_Clear(&cfg);
+      return 1;     
+      } |]
+  case r of 0 -> pure ()
+            _ -> error "Failed to initialize interpreter"
 
 
-py_new_PyConfig :: IO (Ptr ())
-py_new_PyConfig = [C.block|
-  void* {
-    PyConfig* cfg = malloc(sizeof(PyConfig));
-    PyConfig_InitPythonConfig(cfg);
-    return cfg;
-  }|]
-  
-py_InitializeFromConfig :: Ptr () -> IO C.CInt
-py_InitializeFromConfig cfg =
-  [C.block| int {
-          PyStatus status;
-          status = Py_InitializeFromConfig($(void * cfg));
-          return PyStatus_Exception(status);
-          } |]
+finalizePython :: IO ()
+finalizePython = [C.exp| void { Py_Finalize(); } |]
+
+withPython :: IO a -> IO a
+withPython = bracket_ initializePython finalizePython
 
 
 py_Foo :: IO ()
 py_Foo =
   [C.block| void {
-          PyRun_SimpleString("from time import time,ctime\n"
-                             "print('Today is', ctime(time()))\n");
+          PyRun_SimpleString("import sys\n"
+                             "print(sys.argv)\n");
                              }
                              |]
+    
+ 
+withWCtring :: String -> (Ptr CWchar -> IO a) -> IO a
+withWCtring = withArray0 (CWchar 0) . map (fromIntegral . ord)
