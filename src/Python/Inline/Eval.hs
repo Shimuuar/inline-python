fc42596a0d7bddbe8d6cb17f7ca8e5284831d41a
Alexey Khudyakov <alexey.skladnoy@gmail.com>
2025-04-30 15:54:32 +0300

    Add function for populating DictBinder

--- src/Python/Inline/Eval.hs
+++ src/Python/Inline/Eval.hs
@@ -13,6 +13,7 @@ module Python.Inline.Eval
   , codeFromText
   , codeFromString
   , DictBinder
+  , bindVar
     -- * Variable namespaces
   , Namespace(..)
   , Main(..)
@@ -21,5 +22,38 @@ module Python.Inline.Eval
   , Module(..)
   ) where
 
+import Data.ByteString.Unsafe     qualified as BS
+import Data.Text                  (Text)
+import Data.Text.Encoding         qualified as T
+import Language.C.Inline          qualified as C
+import Language.C.Inline.Unsafe   qualified as CU
+
 import Python.Internal.Types
 import Python.Internal.Eval
+import Python.Internal.Program
+import Python.Inline.Literal
+
+
+----------------------------------------------------------------
+C.context (C.baseCtx <> pyCtx)
+C.include "<inline-python.h>"
+----------------------------------------------------------------
+
+-- | Bind variable in dictionary
+bindVar
+  :: (ToPy a)
+  => Text -- ^ Variable name
+  -> a    -- ^ Variable value
+  -> DictBinder
+bindVar name a = DictBinder $ \p_dict -> runProgram $ do
+  p_key <- progIOBracket $ BS.unsafeUseAsCString (T.encodeUtf8 name)
+  p_obj <- takeOwnership =<< progPy (throwOnNULL =<< basicToPy a)
+  progPy $ do
+    r <- Py [CU.block| int {
+      PyObject* p_obj = $(PyObject* p_obj);
+      return PyDict_SetItemString($(PyObject* p_dict), $(char* p_key), p_obj);
+      } |]
+    case r of
+      0 -> pure ()
+      _ -> mustThrowPyError
+
