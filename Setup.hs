a82811df7672aeda603aadb12f3bd36e7b0daf64
Alexey Khudyakov <alexey.skladnoy@gmail.com>
2026-07-28 00:21:02 +0300

    Only use python3-config if cabal flag is set
    By default use pkg-config

--- Setup.hs
+++ Setup.hs
@@ -1,4 +1,5 @@
 {-# LANGUAGE OverloadedRecordDot #-}
+{-# LANGUAGE OverloadedStrings   #-}
 import Data.Either
 
 import Distribution.Simple
@@ -25,34 +26,36 @@ patchedConfHook
   :: (GenericPackageDescription, HookedBuildInfo)
   -> ConfigFlags
   -> IO LocalBuildInfo
-patchedConfHook (gpd, hbi) flags = do
-  cflags_raw  <- readProcess "python3-config" ["--cflags"] ""
-  ldflags_raw <- readProcess "python3-config" ["--embed", "--ldflags"] ""
-  -- Split flags
-  let (inc_dirs,cflags) = partitionEithers
-        [ case flag of
-            '-':'I':path -> Left  path
-            _            -> Right flag
-        | flag <- tokenizeArguments cflags_raw
-        ]
-      (ldflags, (libs, lib_dirs)) = fmap partitionEithers $ partitionEithers
-        [ case flag of
-            '-':'l':path -> Right (Left  path)
-            '-':'L':path -> Right (Right path)
-            _            -> Left flag
-        | flag <- tokenizeArguments ldflags_raw
-        ]
-  let tweakLib lib = case lib.libName of
-        LMainLibName -> lib & L.ccOptions    %~ (++ cflags)
-                            & L.includeDirs  %~ (++ inc_dirs)
-                            & L.ldOptions    %~ (++ ldflags)
-                            & L.extraLibs    %~ (++ libs)
-                            & L.extraLibDirs %~ (++ lib_dirs)
-        _            -> lib
-  let lib' = (fmap . fmap) tweakLib (condLibrary  gpd)
-  let gpd' = gpd { condLibrary = lib' }
-  confHook simpleUserHooks (gpd', hbi) flags
-
+patchedConfHook (gpd, hbi) flags
+  | Just True <- lookupFlagAssignment ("python3-config") $ configConfigurationsFlags flags
+  = do cflags_raw  <- readProcess "python3-config" ["--cflags"] ""
+       ldflags_raw <- readProcess "python3-config" ["--embed", "--ldflags"] ""  
+       -- Split flags
+       let (inc_dirs,cflags) = partitionEithers
+             [ case flag of
+                 '-':'I':path -> Left  path
+                 _            -> Right flag
+             | flag <- tokenizeArguments cflags_raw
+             ]
+           (ldflags, (libs, lib_dirs)) = fmap partitionEithers $ partitionEithers
+             [ case flag of
+                 '-':'l':path -> Right (Left  path)
+                 '-':'L':path -> Right (Right path)
+                 _            -> Left flag
+             | flag <- tokenizeArguments ldflags_raw
+             ]
+       let tweakLib lib = case lib.libName of
+             LMainLibName -> lib & L.ccOptions    %~ (++ cflags)
+                                 & L.includeDirs  %~ (++ inc_dirs)
+                                 & L.ldOptions    %~ (++ ldflags)
+                                 & L.extraLibs    %~ (++ libs)
+                                 & L.extraLibDirs %~ (++ lib_dirs)
+             _            -> lib
+       let lib' = (fmap . fmap) tweakLib (condLibrary  gpd)
+       let gpd' = gpd { condLibrary = lib' }
+       confHook simpleUserHooks (gpd', hbi) flags
+  | otherwise = do
+      confHook simpleUserHooks (gpd, hbi) flags
 
 tokenizeArguments :: String -> [String]
 tokenizeArguments = words
